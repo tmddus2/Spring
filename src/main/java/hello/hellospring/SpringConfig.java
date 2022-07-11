7895cc5cee14193e9024afd92f6d2add4cdaadd0
tmddus2 <49530253+tmddus2@users.noreply.github.com>
2022-07-11 14:24:14 +0900

    Add files via upload

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    //@Bean
+    //public TimeTraceAop timeTraceAop() {
+    //    return new TimeTraceAop();
+    //}
+
+}
